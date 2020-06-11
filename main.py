131dd638e748627487cf0d6d929a5aef6223691c
Guilherme David <davidguilherme.david@gmail.com>
2020-06-11 17:11:15 -0300

    bala

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import pygame
 import random
+import math
+from pygame import mixer
 
-# Inicializa o pygame
+# Inicializa o pygame e o mixer
 pygame.init()
+mixer.init()
 
 # Criar tela com 800x600 pixels de resolução
 screen = pygame.display.set_mode((800, 600))
@@ -11,8 +14,12 @@ screen = pygame.display.set_mode((800, 600))
 background = pygame.image.load('background.png')
 background = pygame.transform.scale(background, (800, 600)) # resize image to 800x600
 
+# Background Sound
+mixer.music.load('background.mp3')
+mixer.music.play(-1)
+
 # Título e Ícone
-pygame.display.set_caption("Xoxota Invaders")
+pygame.display.set_caption("Bota Invaders")
 icon = pygame.image.load('penis.png')
 pygame.display.set_icon(icon)
 
@@ -24,11 +31,19 @@ playerY = 480
 playerX_change = 0
 
 # Inimigo
-enemyImg = pygame.image.load('vagina.png')
-enemyX = random.randint(0,800)
-enemyY = random.randint(50,150)
-enemyX_change = 3 # velocidade de mudança horizontal
-enemyY_change = 40
+enemyImg = []
+enemyX = []
+enemyY = []
+enemyX_change = []
+enemyY_change = []
+num_of_enemies = 6
+
+for i in range(num_of_enemies):
+    enemyImg.append(pygame.transform.scale(pygame.image.load('enemy.png'), (78, 64)))
+    enemyX.append(random.randint(0,735))
+    enemyY.append(random.randint(50,150))
+    enemyX_change.append(2) # velocidade de mudança horizontal
+    enemyY_change.append(40)
 
 # Bullet
 # ready - você não pode ver a bala na tela
@@ -41,18 +56,44 @@ bulletX_change = 0
 bulletY_change = 4
 bullet_state = "ready"
 
+# Pontuação
+score_value = 0
+font = pygame.font.Font('freesansbold.ttf', 32)
+
+textX = 10
+textY = 10
+
+# Texto de Game Over
+over_font = pygame.font.Font('freesansbold.ttf', 64)
+
+def show_score(x,y):
+    score = font.render("Botadas: " + str(score_value), True, (0,0,0))
+    screen.blit(score, (x, y))
+
+def game_over_text():
+    over_text = over_font.render("NÃO BOTASTE", True, (0,0,0))
+    screen.blit(over_text, (200, 250))
+
 # Desenhar jogador na tela
 def player(x, y):
     screen.blit(playerImg, (x, y))
 
 # Desenhar inimigo na tela
-def enemy(x, y):
-    screen.blit(enemyImg, (x, y))
+def enemy(x, y, i):
+    screen.blit(enemyImg[i], (x, y))
 
 def fire_bullet(x,y):
     global bullet_state
     bullet_state = "fire"
     screen.blit(bulletImg, (x+16, y+10))
+
+def isCollision(enemyX, enemyY, bulletX, bulletY):
+    # Distância entre duas coordenadas abaixo
+    distance = math.sqrt(math.pow(enemyX-bulletX, 2) + math.pow(enemyY - bulletY, 2))
+    if distance < 27:
+        return True
+    else:
+        return False
 
 # Game Loop
 running = True
@@ -68,6 +109,8 @@ while running:
             if event.key == pygame.K_RIGHT:
                 playerX_change = 5
             if event.key == pygame.K_SPACE:
+                bullet_sound = mixer.Sound('bullet.wav')
+                bullet_sound.play()
                 if bullet_state is "ready":
                     # Pega a coordenada X atual da rola
                     bulletX = playerX
@@ -85,40 +128,52 @@ while running:
     # Atribuir valor de posição mudado pra coordenada X
     playerX += playerX_change
 
-    # Adicionando limites ao player pra ele não escapar da tela
-    if playerX <= 0:
-        playerX = 0
-    elif playerX >= 736:  # 800 - 64, que é o tamanho da sprite da rola
-        playerX = 736
-
-    # mesmo que acima só que pra o inimigo
-    if enemyX <= 0:
-        enemyX = 0
-    elif enemyX >= 736:  # 800 - 64, que é o tamanho da sprite da rola
-        enemyX = 736
-
     # Movimento do inimigo com bate e volta nas laterais
-    enemyX += enemyX_change
+    for i in range(num_of_enemies):
 
-    if enemyX <= 0:
-        enemyX_change = 4
-        enemyY += enemyY_change # adiciona pixels toda vez q colide
-    elif enemyX >= 736:  # 800 - 64, que é o tamanho da sprite da rola
-        enemyX_change = -4
-        enemyY += enemyY_change # adiciona pixels toda vez q colide
+        # Game Over
+        if enemyY[i] > 440:
+            for j in range(num_of_enemies):
+                enemyY[j] = 2000 # inimigos somem da tela
+            game_over_text()
+            break
+
+        enemyX[i] += enemyX_change[i]
+        if enemyX[i] <= 0:
+            enemyX_change[i] = 2
+            enemyY[i] += enemyY_change[i] # adiciona pixels toda vez q colide
+        elif enemyX[i] >= 736:  # 800 - 64, que é o tamanho da sprite da rola
+            enemyX_change[i] = -2
+            enemyY[i] += enemyY_change[i] # adiciona pixels toda vez q colide
+
+        # Colisão
+        collision = isCollision(enemyX[i], enemyY[i], bulletX, bulletY)
+        if collision:
+            explosion_sound = mixer.Sound('explosion.wav')
+            explosion_sound.play()
+            bulletY = 480
+            bullet_state = "ready"
+            score_value += 1
+            enemyX[i] = random.randint(0, 735)
+            enemyY[i] = random.randint(50, 150)
+
+        # Iniciar inimigo
+        enemy(enemyX[i], enemyY[i], i)
 
     # Movimento da bala
     if bulletY <= 0:
         bulletY = 480
         bullet_state = "ready"
+
     if bullet_state is "fire":
         fire_bullet(bulletX, bulletY)
         bulletY -= bulletY_change
 
     # Iniciar o player com as posições iniciais, função definida lá em cima
     player(playerX, playerY)
-    # Iniciar inimigo
-    enemy(enemyX, enemyY)
+
+    # mostrar pontuação na tela
+    show_score(textX, textY)
 
     # Tem que atualizar a tela no loop se não vai ficar a mesma coisa
     pygame.display.update()
